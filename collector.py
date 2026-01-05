76ae9f41dbaf1da5474e4a887ad4e2817539ae00
Itsllzx <m.h185h185@gmail.com>
2026-01-05 14:48:35 +0330

    Update collector.py

--- collector.py
+++ collector.py
@@ -3,26 +3,26 @@ import re
 
 # سورس‌های مستقیم و تست شده
 SOURCES = [
-https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt,
-https://raw.githubusercontent.com/barry-far/V2ray-config/main/Sub1.txt,
-https://raw.githubusercontent.com/barry-far/V2ray-config/main/Sub2.txt,
-https://raw.githubusercontent.com/hans-thomas/v2ray-subscription/master/servers.txt,
-https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/v2ray/all_sub.txt,
-https://raw.githubusercontent.com/ninjastrikers/v2ray-configs/main/Sub1.txt,
-https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/all_extracted_configs.txt,
-https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/vmess.txt,
-https://raw.githubusercontent.com/Net-Account/Config/main/All.txt,
-https://raw.githubusercontent.com/M-Mashreghi/Free-V2ray-Collector/main/shuffle/Sub1_shuffled.conf,
-https://raw.githubusercontent.com/mrvcoder/V2rayCollector/main/vmess_iran.txt,
-https://raw.githubusercontent.com/hamed1124/port-based-v2ray-configs/main/sub.txt,
-https://raw.githubusercontent.com/ShatakVPN/ConfigForge-V2Ray/main/configs.txt,
-https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Sub.txt,
-https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/super-sub.txt,
-https://raw.githubusercontent.com/ninjastrikers/v2ray-configs/main/Sub20.txt,
-https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_base64_Sub.txt,
-https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/vless_configs.txt,
-https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/warp.txt,
-https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/hy2.txt
+"https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt",
+"https://raw.githubusercontent.com/barry-far/V2ray-config/main/Sub1.txt",
+"https://raw.githubusercontent.com/barry-far/V2ray-config/main/Sub2.txt",
+"https://raw.githubusercontent.com/hans-thomas/v2ray-subscription/master/servers.txt",
+"https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/v2ray/all_sub.txt",
+"https://raw.githubusercontent.com/ninjastrikers/v2ray-configs/main/Sub1.txt",
+"https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/all_extracted_configs.txt",
+"https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/vmess.txt",
+"https://raw.githubusercontent.com/Net-Account/Config/main/All.txt",
+"https://raw.githubusercontent.com/M-Mashreghi/Free-V2ray-Collector/main/shuffle/Sub1_shuffled.conf",
+"https://raw.githubusercontent.com/mrvcoder/V2rayCollector/main/vmess_iran.txt",
+"https://raw.githubusercontent.com/hamed1124/port-based-v2ray-configs/main/sub.txt",
+"https://raw.githubusercontent.com/ShatakVPN/ConfigForge-V2Ray/main/configs.txt",
+"https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Sub.txt",
+"https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/super-sub.txt",
+"https://raw.githubusercontent.com/ninjastrikers/v2ray-configs/main/Sub20.txt",
+"https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_base64_Sub.txt",
+"https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/vless_configs.txt",
+"https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/warp.txt",
+"https://raw.githubusercontent.com/Joker-funland/V2ray-configs/main/hy2.txt"
 
 ]
 
